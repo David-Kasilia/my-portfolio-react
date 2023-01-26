12a48bea8044489a7e6fe31806a481d2432ab29a
David-Kasilia <davidkasilia001@gmail.com>
2023-01-27 02:58:48 +0300

    Complete contact me page

--- src/components/ContactMe/Contact.js
+++ src/components/ContactMe/Contact.js
@@ -16,14 +16,6 @@ const Contact = () => (
       </h5>
     </div>
     <ContactForm />
-    {/* <div className="row">
-      <div className="col">
-        <input type="text" className="form-control" placeholder="First name" aria-label="First name" />
-      </div>
-      <div className="col">
-        <input type="text" className="form-control" placeholder="Last name" aria-label="Last name" />
-      </div>
-    </div> */}
   </div>
 );
 
--- src/components/ContactMe/FormSpree.js
+++ src/components/ContactMe/FormSpree.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-restricted-globals */
 /* eslint-disable import/no-extraneous-dependencies */
 /* eslint-disable jsx-a11y/label-has-associated-control */
 import React from 'react';
@@ -6,36 +7,66 @@ import { useForm, ValidationError } from '@formspree/react';
 function ContactForm() {
   const [state, handleSubmit] = useForm('mpzbvrag');
   if (state.succeeded) {
-    return <p>Thanks for joining!</p>;
+    return (
+      <div className="alert alert-success mt-5 w-50 ms-5" role="alert">
+        Thank You For Contacting David Kasilia!!
+        <div className="spinner-grow text-success float-end" role="status">
+          <span className="visually-hidden">Loading...</span>
+        </div>
+      </div>
+    );
   }
   return (
-    <form onSubmit={handleSubmit}>
-      <label htmlFor="email">
-        Email Address
-      </label>
-      <input
-        id="email"
-        type="email"
-        name="email"
-      />
-      <ValidationError
-        prefix="Email"
-        field="email"
-        errors={state.errors}
-      />
-      <textarea
-        id="message"
-        name="message"
-      />
-      <ValidationError
-        prefix="Message"
-        field="message"
-        errors={state.errors}
-      />
-      <button type="submit" disabled={state.submitting}>
-        Submit
-      </button>
-    </form>
+    <div className="contact-details">
+      <form onSubmit={handleSubmit}>
+        <div className="row mt-5">
+          <div className="col">
+            <label className="form-label text-white">
+              First Name
+            </label>
+            <input type="text" className="form-control" placeholder="First name" aria-label="First name" />
+          </div>
+          <div className="col">
+            <label className="form-label text-white">
+              Last Name
+            </label>
+            <input type="text" className="form-control" placeholder="Last name" aria-label="Last name" />
+          </div>
+        </div>
+        <div className="mb-3 mt-2">
+          <label htmlFor="email" className="form-label text-white">
+            Email Address
+          </label>
+          <input
+            id="email"
+            type="email"
+            name="email"
+            className="form-control"
+          />
+          <ValidationError
+            prefix="Email"
+            field="email"
+            errors={state.errors}
+          />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="exampleFormControlTextarea1" className="form-label text-white">Enter Your Message</label>
+          <textarea
+            id="message"
+            name="message"
+            className="form-control"
+          />
+          <ValidationError
+            prefix="Message"
+            field="message"
+            errors={state.errors}
+          />
+        </div>
+        <button type="submit" className=" btn btn-danger" disabled={state.submitting} onClick={() => setTimeout(() => { location.reload(); }, 5500)}>
+          Submit
+        </button>
+      </form>
+    </div>
   );
 }
 
