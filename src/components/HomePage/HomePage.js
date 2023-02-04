b733bc359c5289d1ae05d2259b3be0f7b1f33e8e
David-Kasilia <davidkasilia001@gmail.com>
2023-02-04 20:34:37 +0300

    Add effects to home page

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -1,35 +1,48 @@
+/* eslint-disable max-len */
 import React from 'react';
-import Image from '../../assets/images/myImage.jpg';
+// import Image from '../../assets/images/myImage.jpg';
 
 const HomePage = () => (
   <div className="container-fluid homepage">
-    <div className="card mb-3 home-card">
-      <div className="row g-0">
-        <div className="col-md-4">
-          <img src={Image} className="img-fluid rounded-start profile" alt="David Kasilia Profile" />
-        </div>
-        <div className="col-md-8">
-          <div className="card-body home-card-body">
-            <h5 className="card-title ms-5 fs-1 fw-bold text-white">DAVID KASILIA MWANZIA</h5>
-            <p className="card-text ms-5 fs-3 mt-3 fw-semibold text-white">
-              Full Stack Web Developer,
-              <br />
-              Associate Android Developer,
-              <br />
-              & Graffiti Artist
-            </p>
-            <p className="card-text ms-5">
-              <small className=" fs-6 fw-semibold text-white">
-                With Passion For:
-                <br />
-                Creating High Quality UI & Great UX Websites & Android Applications
-              </small>
-            </p>
-          </div>
-        </div>
+    <div className="name-container">
+      <h1 className="fw-bold name">
+        <span>
+          DAVID KASILIA MWANZIA
+        </span>
+      </h1>
+    </div>
+    <div className="home-card">
+      <div className="opening-text">
+        <h3 className="fw-bold home-title">
+          Full Stack Web Developer,
+          <br />
+          Associate Android Developer,
+          <br />
+          & Graffiti Artist
+        </h3>
       </div>
     </div>
   </div>
 );
 
 export default HomePage;
+
+// {/* <img src={Image} className="img-fluid rounded-start profile" alt="David Kasilia Profile" /> */}
+// {/* <div className="col-md-8">
+// <div className="card-body home-card-body">
+//   <p className="card-text ms-5 fs-3 mt-3 fw-semibold text-white">
+//     Full Stack Web Developer,
+//     <br />
+//     Associate Android Developer,
+//     <br />
+//     & Graffiti Artist
+//   </p>
+//   <p className="card-text ms-5">
+//     <small className=" fs-6 fw-semibold text-white">
+//       With Passion For:
+//       <br />
+//       Creating High Quality UI & Great UX Websites & Android Applications
+//     </small>
+//   </p>
+// </div>
+// </div> */}
