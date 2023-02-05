1c63b3c6d133a6d72dd2d95343d78d816b620ce2
David-Kasilia <davidkasilia001@gmail.com>
2023-02-05 18:35:38 +0300

    Updated projects description

--- src/components/Projects/Project.js
+++ src/components/Projects/Project.js
@@ -2,7 +2,7 @@ const Project = [
   {
     id: 1,
     name: 'Movie Mania Entertainment',
-    description: 'This is a front-end web application for checking Movie and Series updates. It shows the movies currently playing in theatre, popular movies and series suggestions with their descriptions and genre.',
+    description: 'Today many people are entertained by movies and series. This is a front-end web application for checking Movie and Series updates. It shows the movies currently playing in theatre, popular movies and series suggestions with their descriptions and genre. This website fetches data from a RESTFul TMDB API(The Movie Database). It is purely built using React and Redux and styling of the project is done by CSS3, ',
     image: 'https://www.linkpicture.com/q/all-devices-black-3.png',
     technologies: 'React, Redux, CSS3',
     live: 'https://movie-mania-kasilia-creations.netlify.app/',
@@ -11,7 +11,7 @@ const Project = [
   {
     id: 2,
     name: 'Bookstore CMS',
-    description: 'This is a MVP version of it that allows the user to: Display a list of books, Add a book, Remove a selected book.',
+    description: 'Are you a book enthusiast? This is a MVP version of it that allows the user to: Display a list of books, Add a book, Remove a selected book. This website is built using React and Redux. Styling is done purely by CSS3. It also fetches data from a RESTFul API.',
     image: 'https://www.linkpicture.com/q/all-devices-black-5.png',
     technologies: 'React, Redux, CSS3',
     live: 'https://bookstore-cms-kasilia-creations.netlify.app',
@@ -20,7 +20,7 @@ const Project = [
   {
     id: 3,
     name: 'Leaderboard',
-    description: 'This is JavaScript project for the Leaderboard list app, using webpack and ES6 features, notably modules.',
+    description: 'Do you like Ranking the best Mortal Kombat player? This is JavaScript project for the Leaderboard list app, using webpack and ES6 features, notably modules. This project shows a list of the best players ranked using their scores. It fetches and updates data using a RESTFul API. The styling is done by pure CSS3.',
     image: 'https://www.linkpicture.com/q/all-devices-black-1.png',
     technologies: 'HTML, CSS, JavaScript, Webpack',
     live: 'https://mortal-kombat-leaderboard.netlify.app/',
@@ -28,17 +28,17 @@ const Project = [
   },
   {
     id: 4,
-    name: 'Budget APP',
-    description: 'This is building a mobile web application where you can manage your budget: you have a list of transactions associated with a category, so that you can see how much money you spent and on what.',
-    image: 'https://www.linkpicture.com/q/all-devices-black-9.png',
-    technologies: 'Ruby, Ruby On Rails, PostgreSQL',
-    live: ' https://budget-app-5474.onrender.com',
-    source: 'https://github.com/David-Kasilia/budget_app_ror',
+    name: 'Math Magicians',
+    description: 'Mathematics made easier!"Math magicians" is a website for all fans of mathematics. It is a Single Page App (SPA) that allows users to: Make simple calculations and Read a random math-related quote. This website is built using React Redux and styling is done using CSS3.',
+    image: 'https://www.linkpicture.com/q/all-devices-black_1.png',
+    technologies: 'React, Redux, CSS3',
+    live: 'https://math-magicians-kasilia-creations.netlify.app/',
+    source: 'https://github.com/David-Kasilia/math-magician-with-react',
   },
   {
     id: 5,
     name: 'Greetings',
-    description: 'This is a full stacked website which fetches data from an RESTFul API and renders greetings to the front-end',
+    description: 'This is a full stacked website which fetches data from an RESTFul API and renders greetings to the front-end. This project backend is built using Ruby On Rails and the frontend is built using react and Redux. as for the project styling I used Bootstrap 5',
     image: 'https://www.linkpicture.com/q/all-devices-white-2.png',
     technologies: 'React, Redux Ruby On Rails, PostgreSQL',
     live: 'https://hello-app-react-kasiliacreations.netlify.app',
@@ -48,7 +48,7 @@ const Project = [
   {
     id: 6,
     name: '3D Website',
-    description: 'This Website is about, 3D technology (three-dimensional). Built with HTML and CSS only',
+    description: 'Have you ever seen a 3D Background image on a website? This a frontend Website about, 3D technology (three-dimensional). It is built with HTML5 and CSS3 only',
     image: 'https://www.linkpicture.com/q/all-devices-black-7.png',
     technologies: 'HTML, CSS, JavaScript',
     live: 'https://3d-website-kasilia-creations.netlify.app',
@@ -57,7 +57,7 @@ const Project = [
   {
     id: 7,
     name: 'Metrics APP: Countries',
-    description: 'This React capstone project is about building a mobile web application to check a list of metrics (numeric values) that you will create making use of React and Redux.',
+    description: 'Do You know the statistics of countries of the world? This React capstone project is about building a mobile web application to check a list of metrics (numeric values) that you will create making use of React and Redux. The project fetches data from a RESTFul API that has all the details about countries of the world.',
     image: 'https://www.linkpicture.com/q/all-devices-black-4.png',
     technologies: 'React, Redux',
     live: 'https://metrics-countries-kasilia-creations.netlify.app/',
@@ -66,7 +66,7 @@ const Project = [
   {
     id: 8,
     name: 'Mobile App Landing Page',
-    description: 'HTML/CSS & JavaScript First Microverse Capstone Project. This is a website for a mobile application landing page built with HTML, CSS and JavaScript',
+    description: 'This was a HTML/CSS & JavaScript First Microverse Capstone Project. This is a website for a mobile application landing page. It has details about the application being built and details about the company that built the mobile application. The website is built with HTML, CSS and JavaScript',
     image: 'https://www.linkpicture.com/q/all-devices-black-8.png',
     technologies: 'HTML, CSS, JavaScript',
     live: 'https://www.google.com',
@@ -75,7 +75,7 @@ const Project = [
   {
     id: 9,
     name: 'Lamborghini Website Figma',
-    description: 'This is a clone of the Lamborghini website User Interface Built with Figma',
+    description: 'To illustrate my skills in UI design, I decided to clone the homepage of Lamborghini website. This clone of User Interface Built with Figma.',
     image: 'https://www.linkpicture.com/q/Lamborghini-Clone-UI.png',
     technologies: 'Figma',
     live: 'https://www.figma.com/file/zJUWspmkSGkUIVe8VsCPf4/Lamborghini-Clone-UI?node-id=0%3A1&t=JAudJC4oZxZTRq84-1',
@@ -84,7 +84,7 @@ const Project = [
   {
     id: 10,
     name: 'Sneakers Android Application',
-    description: 'There are a number of sneakers. This is an Android Application that has a list of popular sneakers and their relevant prices. You can add your sneakers to your favourite list of sneakers in the app.',
+    description: 'There are a number of sneakers. Most of the people don\'t know the types of sneakers and their brands . This is an Android Application that has a list of popular sneakers and their relevant prices. You can add your sneakers to your favorite list of sneakers in the app.',
     image: 'https://www.linkpicture.com/q/sfsfsffsdfsfsdfsd.png',
     technologies: 'Kotlin',
     live: 'https://github.com/David-Kasilia/Sneakers-Application',
